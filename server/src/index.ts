3710e89411221d2f649c1230ec0f46942d329c69
nartrung <nartrung02@gmail.com>
2024-01-29 16:52:24 +0700

    Added Async-error Handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import "express-async-errors";
 import "./db";
 import authRouter from "./routes/authRouter";
 import audioRouter from "./routes/audioRouter";
@@ -8,6 +9,7 @@ import playlistRouter from "./routes/playlistRouter";
 import profileRouter from "./routes/profileRouter";
 import historyRouter from "./routes/historyRouter";
 import "./utils/schedule";
+import { errorHandler } from "./middlewares/async-error";
 
 const app = express();
 const PORT = 8080;
@@ -21,6 +23,7 @@ app.use("/favorite", favoriteRouter);
 app.use("/playlist", playlistRouter);
 app.use("/profile", profileRouter);
 app.use("/history", historyRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("\u001b[1;32mApp is running at port " + PORT);
