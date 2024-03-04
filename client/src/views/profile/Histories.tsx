10a6fefe6495c78ccc00c7b1391634bd398a79c3
nartrung <nartrung02@gmail.com>
2024-03-04 17:55:00 +0700

    feature: Add delete single audio and all audio from history

--- client/src/views/profile/Histories.tsx
+++ client/src/views/profile/Histories.tsx
@@ -1,16 +1,92 @@
 import AudioLoadingUI from '@ui/AudioLoadingUI';
 import LoadingAnimation from '@ui/LoadingAnimation';
 import colors from '@utils/colors';
-import {FC} from 'react';
-import {View, StyleSheet, ScrollView, Pressable} from 'react-native';
+import {FC, useState} from 'react';
+import {View, StyleSheet, ScrollView, Pressable, Alert} from 'react-native';
 import {Text} from 'react-native';
 import {FetchHistories} from 'src/hooks/query';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import axios from 'axios';
+import {getDataFromAsyncStorage, keys} from '@utils/asyncStorage';
+import Toast from 'react-native-toast-message';
+import {useQueryClient} from 'react-query';
+import Loader from '@ui/Loader';
 
 interface Props {}
 
 const Histories: FC<Props> = props => {
+  const [busy, setBusy] = useState(false);
   const {data, isLoading} = FetchHistories();
+  const queryClient = useQueryClient();
+
+  const clearHistory = async () => {
+    try {
+      const token = await getDataFromAsyncStorage(keys.AUTH_TOKEN);
+      const {data} = await axios.delete(
+        'http://10.0.2.2:8080/history?all=yes',
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        },
+      );
+      queryClient.invalidateQueries({queryKey: ['histories']});
+      if (data) {
+        Toast.show({
+          type: 'info',
+          text1: 'Đã xóa toàn bộ lịch sử',
+        });
+      }
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Đã có lỗi xảy ra! Vui lòng thử lại',
+      });
+    }
+  };
+  const clearSingleAudio = async (audioID: string) => {
+    setBusy(true);
+    try {
+      const token = await getDataFromAsyncStorage(keys.AUTH_TOKEN);
+      const {data} = await axios.delete(
+        'http://10.0.2.2:8080/history?audioId=' + audioID,
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        },
+      );
+      queryClient.invalidateQueries({queryKey: ['histories']});
+      if (data) {
+        Toast.show({
+          type: 'success',
+          text1: 'Xóa thành công',
+        });
+      }
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Đã có lỗi xảy ra! Vui lòng thử lại',
+      });
+    }
+    setBusy(false);
+  };
+  const handleClearAll = () => {
+    return Alert.alert(
+      'Xóa toàn bộ lịch sử nghe',
+      'Bạn có chắc muốn xóa toàn bộ lịch sử?',
+      [
+        {
+          text: 'Đóng',
+        },
+        {
+          text: 'Xóa toàn bộ',
+          onPress: clearHistory,
+        },
+      ],
+    );
+  };
+
   if (isLoading)
     return (
       <LoadingAnimation>
@@ -19,7 +95,12 @@ const Histories: FC<Props> = props => {
     );
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.sectionTitle}>Lịch sử nghe</Text>
+      <View style={styles.titleAndDel}>
+        <Text style={styles.sectionTitle}>Lịch sử nghe</Text>
+        <Pressable onPress={handleClearAll}>
+          <Text style={styles.del}>Xóa tất cả</Text>
+        </Pressable>
+      </View>
       {data?.map((item, index) => {
         return (
           <View key={item.date + index}>
@@ -28,9 +109,13 @@ const Histories: FC<Props> = props => {
               return (
                 <View key={audio.id + index} style={styles.history}>
                   <Text style={styles.historyTitle}>{audio.title}</Text>
-                  <Pressable>
-                    <AntDesign name="close" size={24} />
-                  </Pressable>
+                  {busy ? (
+                    <Loader />
+                  ) : (
+                    <Pressable onPress={() => clearSingleAudio(audio.id)}>
+                      <AntDesign name="close" size={24} />
+                    </Pressable>
+                  )}
                 </View>
               );
             })}
@@ -49,6 +134,16 @@ const styles = StyleSheet.create({
     color: colors.CONTRAST,
     fontFamily: 'opensans_bold',
     fontSize: 20,
+  },
+  titleAndDel: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  del: {
+    fontFamily: 'opensans_bold',
+    color: colors.ERROR,
+    fontSize: 16,
   },
   date: {
     color: colors.PRIMARY,
